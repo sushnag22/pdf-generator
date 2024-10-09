9c8a5d2ead4741a252dc6058780101575bfe287a
Sushruth Nagaraj <sushruthnagaraj2001@gmail.com>
2024-10-10 00:12:23 +0530

    Initial commit

---
+++ src/test/java/io/github/sushnag22/pdfgenerator/PdfGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.sushnag22.pdfgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
